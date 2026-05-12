d9bf29a7d5eab3cb0e8f239dec7479397a5f7c0a
Tom Hipsh <33371239+TomHipsh@users.noreply.github.com>
2026-05-12 16:40:33 +0300

    typescript setup
    typescript setup

---
+++ src/pages/SearchPage/index.tsx
@@ -0,0 +1,14 @@
+function SearchPage() {
+  return (
+    <main
+      aria-label="Search page"
+      style={{
+        minHeight: '100vh',
+        width: '100%',
+        backgroundColor: '#fff',
+      }}
+    />
+  );
+}
+
+export default SearchPage;
